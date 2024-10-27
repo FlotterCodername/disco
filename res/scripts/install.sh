4758f4c4125e08738c1d2908e15ae5c6fb89c3ce
David <80349258+DavidSiegl@users.noreply.github.com>
2024-10-27 13:22:59 +0100

    Update res/scripts/install.sh
    Co-authored-by: Fabian H. Schneider <4962476+FlotterCodername@users.noreply.github.com>

--- res/scripts/install.sh
+++ res/scripts/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+# Copyright © 2024 David Siegl
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://mozilla.org/MPL/2.0/.
 usage() {
   echo "Usage: $0 -s SERVICE_NAME -i IMAGE_NAME -v VOLUME_PATH [-p HOST_PORT:CONTAINER_PORT] [-r REPLICAS]"
   exit 1
