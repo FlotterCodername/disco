dc8229ee913ccb0723bcafcce8ad9692d37f66f9
David <80349258+DavidSiegl@users.noreply.github.com>
2024-10-27 13:23:07 +0100

    Update res/scripts/uninstall.sh
    Co-authored-by: Fabian H. Schneider <4962476+FlotterCodername@users.noreply.github.com>

--- res/scripts/uninstall.sh
+++ res/scripts/uninstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+# Copyright © 2024 David Siegl
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://mozilla.org/MPL/2.0/.
 # Function to display usage instructions
 usage() {
   echo "Usage: $0 -s SERVICE_NAME [-c]"
